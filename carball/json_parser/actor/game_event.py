126e4af726af0a5075cfe626c37de5d77cc3c698
Harry Xie <harry1996@gmail.com>
2020-01-01 12:57:23 +0000

    added more GameEvent ClassNames (for older replays), f

--- carball/json_parser/actor/game_event.py
+++ carball/json_parser/actor/game_event.py
@@ -5,7 +5,11 @@ class GameEventHandler(BaseActorHandler):
 
     @classmethod
     def can_handle(cls, actor: dict) -> bool:
-        return actor['ClassName'].startswith('TAGame.GameEvent_Soccar_TA')
+        return actor['ClassName'].startswith('TAGame.GameEvent_Soccar_TA') \
+               or actor['ClassName'].startswith('TAGame.GameEvent_SoccarPrivate_TA')  # Needed for RLCS S1
+        # May need more ClassNames here, such as TAGame.GameEvent_SoccarSplitscreen_TA
+        # See GameEvents here:
+        # https://github.com/jjbott/RocketLeagueReplayParser/blob/34b2c72fddd24d21c2aa8c5a2e1ac9f85942306c/RocketLeagueReplayParser/NetworkStream/ActorState.cs#L88
 
     def update(self, actor: dict, frame_number: int, time: float, delta: float) -> None:
         self.parser.soccar_game_event_actor = actor
