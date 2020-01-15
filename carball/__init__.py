99990ad2947a151061bdf5268f1139bd195f5a1f
Harry Xie <harry1996@gmail.com>
2020-01-15 20:07:54 +0000

    added generated to PATH to fix protobuf imports

--- carball/__init__.py
+++ carball/__init__.py
@@ -4,3 +4,8 @@ try:
     pass
 except ModuleNotFoundError as e:
     print("Not importing functions due to missing packages:", e)
+
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent / "generated"))
